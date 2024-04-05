a18a2a0ab7fece0a13eaa7433e2a2a7b1891d0de
joelnitta <joelnitta@gmail.com>
2024-04-05 13:09:36 +0000

    Use dynamic targets for BLAST test

--- R/functions.R
+++ R/functions.R
@@ -381,7 +381,341 @@ align_with_og <- function(fern_seqs, marker) {
   align_seqs(c(og_seqs, fern_seqs))
 }
 
+# Drop sequences that are not identified to species
 drop_indets <- function(seq_list) {
   to_drop <- str_detect(names(seq_list), "sp.__")
   seq_list[!to_drop]
+}
+
+#' Drop species complexes and hybrids from sequences
+#'
+#' @param seq_list DNA sequences; List of class DNAbin.
+#' @param sp_complex Vector of species names that are species complexes.
+#' @param hybrid_taxa Vector of specimens that are hybrids.
+#'
+#' @return DNA sequences with species complexes and hybrid taxa removed
+drop_complex_hybrids <- function(seq_list, sp_complex, hybrid_taxa) {
+  is_sp_complex <- str_detect(
+    names(seq_list), paste(sp_complex, collapse = "|"))
+  is_hybrid <- names(seq_list) %in% hybrid_taxa
+  assertthat::assert_that(
+    isTRUE(sum(is_hybrid) <= length(hybrid_taxa))
+  )
+  to_drop <- is_sp_complex | is_hybrid
+  seq_list[!to_drop]
+}
+
+#' Build a BLAST database.
+#'
+#' This is a wrapper for makeblastdb.
+#'
+#' @param in_seqs Character vector of length one; the path to the fasta
+#' file containing the sequences to be included in the database.
+#' @param db_type Character vector of length one; "nucl" for DNA or "prot"
+#' for amino acids (proteins).
+#' @param out_name Character vector of length one; name of BLAST database
+#' to be created (optional). This will be used to name all database files;
+#' if omitted, the name of the `in_seqs` file will be used instead.
+#' @param title Character vector of length one; title of BLAST database
+#' to be created (optional).
+#' @param parse_seqids Logical; should the makeblastdb flag
+#' "parse_seqids" be used?
+#' @param wd Character vector of length one; working directory. The blast
+#' database will be made here.
+#' @param echo Logical; should standard error and output be printed?
+#' @param ... Additional other arguments. Not used by this function, but
+#' meant to be used by \code{\link[drake]{drake_plan}} for tracking
+#' during workflows.
+#' @return A series of files starting with \code{out_name} and ending in
+#' .phr, .pin, .pog, .psd, .psi, psq (for proteins) or .nhr, .nin, .nog,
+#' .nsd, .nsi, and .nsq (for DNA) that constitute the BLAST database in
+#' the working directory.
+#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
+#' @references \url{https://www.ncbi.nlm.nih.gov/books/NBK279690/}
+#' @examples
+#' library(ape)
+#' data(woodmouse)
+#' temp_dir <- fs::dir_create(fs::path(tempdir(), "baitfindR_example"))
+#' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse.fasta"))
+#' list.files(temp_dir)
+#' build_blast_db(
+#'   fs::path(temp_dir, "woodmouse.fasta"),
+#'   title = "test db",
+#'   out_name = "wood",
+#'   parse_seqids = TRUE,
+#'   wd = temp_dir)
+#' list.files(temp_dir)
+#' fs::file_delete(temp_dir)
+#' @export
+build_blast_db <- function(in_seqs,
+                            db_type = "nucl",
+                            out_name = NULL,
+                            title = NULL,
+                            parse_seqids = FALSE,
+                            echo = TRUE,
+                            wd, ...) {
+
+  # Check input
+  assertthat::assert_that(assertthat::is.string(in_seqs))
+  assertthat::assert_that(assertthat::is.string(db_type))
+  assertthat::assert_that(assertthat::is.string(wd))
+  assertthat::assert_that(is.logical(parse_seqids))
+  assertthat::assert_that(assertthat::is.string(title) | is.null(title))
+  assertthat::assert_that(assertthat::is.string(out_name) | is.null(out_name))
+
+  assertthat::assert_that(db_type %in% c("nucl", "prot"))
+
+  wd <- fs::path_abs(wd)
+  assertthat::assert_that(assertthat::is.dir(wd))
+
+  in_seqs <- fs::path_abs(in_seqs)
+  assertthat::assert_that(assertthat::is.readable(in_seqs))
+
+  # Prepare arguments
+  parse_seqids <- if(isTRUE(parse_seqids)) "-parse_seqids" else NULL
+  title <- if(!is.null(title)) c("-title", title) else NULL
+  out_name <- if(!is.null(out_name)) c("-out", out_name) else NULL
+
+  arguments <- c("-in", in_seqs,
+                 "-dbtype", db_type,
+                 parse_seqids,
+                 out_name,
+                 title)
+
+  # run command
+  processx::run("makeblastdb", arguments, wd = wd, echo = echo)
+
+}
+
+write_fasta <- function(x, file, ...) {
+  ape::write.FASTA(x = x, file = file, ...)
+  file
+}
+
+#' Make a blast database and return paths to the created files
+#'
+#' @param seqs_path Path to sequences
+#' @param blast_db_dir Folder to write BLAST database
+#' @param out_name Name of BLAST database
+#'
+#' @return Paths to components of BLAST database.
+#'   Externally, database will be created
+#'
+make_blast_db <- function(seqs_path, blast_db_dir, out_name) {
+
+  # Define output files
+  out_files <- fs::path(blast_db_dir, out_name) %>%
+    paste0(c(".nhr", ".nin", ".nsq"))
+
+  # Delete any existing output
+  for (i in seq_along(out_files)) {
+    if(fs::file_exists(out_files[[i]])) {
+      fs::file_delete(out_files[[i]])
+    }
+  }
+
+  # Create blast DB (side-effect)
+  build_blast_db(
+    seqs_path,
+    title = out_name,
+    out_name = out_name,
+    parse_seqids = FALSE,
+    wd = blast_db_dir)
+
+  # Return path to output file
+  out_files
+
+}
+
+#' Run a blastn query.
+#'
+#' This is a wrapper for blastn.
+#'
+#' @param query Character vector of length one; the path to the fasta
+#' file to use as the query sequence(s).
+#' @param database Character vector of length one; the name of the blast
+#' database.
+#' @param out_file Character vector of length one; the name to use for
+#' the results file.
+#' @param outfmt Character vector of length one; value to pass to
+#' \code{blastn} \code{outfmt} argument. Default = "6".
+#' @param other_args Character vector; other arguments to pass on to
+#' \code{blastn}.
+#' Must be formatted so that each argument name and its value are
+#' separate, consecutive elements of the vector, e.g.,
+#' \code{c("-evalue", 10, "-num_threads", 1)}.
+#' The argument name must be preceded by a hyphen.
+#' For a list of options, run \code{blastn -help}.
+#' @param wd Character vector of length one; working directory. The blast
+#' search will be conducted here.
+#' @param echo Logical; should standard error and output be printed?
+#' @param ... Additional other arguments. Not used by this function,
+#' but meant to be used by \code{\link[drake]{drake_plan}} for tracking
+#' during workflows.
+#' @return A tab-separated text file with the results of the blastn
+#' search, named with the value of \code{out_file}.
+#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
+#' @references \url{https://www.ncbi.nlm.nih.gov/books/NBK279690/}
+#' @examples
+#' library(ape)
+#'
+#' # Make temp dir for storing files
+#' temp_dir <- fs::dir_create(fs::path(tempdir(), "baitfindR_example"))
+#'
+#' # Write out ape::woodmouse dataset as DNA
+#' data(woodmouse)
+#' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse.fasta"))
+#'
+#' # Make blast database
+#' build_blast_db(
+#'   fs::path(temp_dir, "woodmouse.fasta"),
+#'   db_type = "nucl",
+#'   out_name = "wood",
+#'   parse_seqids = TRUE,
+#'   wd = temp_dir)
+#'
+#' # Blast the original sequences against the database
+#' blast_n(
+#'   fs::path(temp_dir, "woodmouse.fasta"),
+#'   database = "wood",
+#'   out_file = "blastn_results",
+#'   wd = temp_dir,
+#'   echo = TRUE
+#' )
+#'
+#' # Take a look at the results.
+#' readr::read_tsv(
+#'   fs::path(temp_dir, "blastn_results"),
+#'   col_names = FALSE
+#'   )
+#'
+#' # Cleanup.
+#' fs::file_delete(temp_dir)
+#' @export
+blast_n <- function(query,
+                     database,
+                     out_file = NULL,
+                     outfmt = "6",
+                     other_args = NULL,
+                     echo = TRUE,
+                     wd,
+                     ...) {
+
+  # Check input
+
+  assertthat::assert_that(assertthat::is.string(query))
+  assertthat::assert_that(assertthat::is.string(database))
+  assertthat::assert_that(assertthat::is.string(out_file) | is.null(out_file))
+  assertthat::assert_that(assertthat::is.string(outfmt))
+  assertthat::assert_that(is.character(other_args) | is.null(other_args))
+  assertthat::assert_that(is.logical(echo))
+  assertthat::assert_that(assertthat::is.string(wd))
+  assertthat::assert_that(
+    length(other_args) > 1 | is.null(other_args),
+    msg = "other_args not formatted correctly.")
+
+  wd <- fs::path_abs(wd)
+  assertthat::assert_that(assertthat::is.dir(wd))
+
+  query <- fs::path_abs(query)
+  assertthat::assert_that(assertthat::is.readable(query))
+
+  # modify arguments
+
+  arguments <- c("-query", query,
+                 "-db", database,
+                 "-outfmt", outfmt,
+                 "-out", out_file,
+                 other_args)
+
+  # run command
+  processx::run("blastn", arguments, wd = wd, echo = echo)
+
+  # return output file
+  fs::path(wd, out_file)
+
+}
+
+load_blast_tsv <- function(blast_out_path, dataset_name) {
+  # Read in sequences.
+  # BLAST doesn't output column headers, so we need to specify
+  # (make sure they match correctly first!)
+  fmt6_cols <- c("qseqid", "sseqid", "pident", "length", "mismatch",
+                 "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore")
+
+  # Read in BLAST output
+  readr::read_tsv(
+    blast_out_path,
+    col_names = fmt6_cols,
+    col_types = "ccdddddddddd" # two ID cols are char, rest is numeric
+  ) %>%
+  mutate(dataset = dataset_name)
+}
+
+prep_blast_res <- function(blast_res_raw) {
+
+  blast_res_raw %>%
+    # select(qseqid, sseqid, pident, evalue, bitscore) %>%
+    filter(qseqid != sseqid) %>%
+    mutate(
+      q_species = str_split(qseqid, "__") %>% map_chr(1)) %>%
+    mutate(
+      s_species = str_split(sseqid, "__") %>% map_chr(1)) %>%
+    group_by(q_species) %>%
+    mutate(n_indiv = n_distinct(qseqid)) %>%
+    ungroup() %>%
+    mutate(
+      comp_type = if_else(q_species == s_species, "intra", "inter")
+    )
+}
+
+get_min_intra_dist <- function(blast_res) {
+  blast_res %>%
+    filter(comp_type == "intra") %>%
+    group_by(s_species) %>%
+    slice_min(pident) %>%
+    ungroup() %>%
+    arrange(pident)
+}
+
+get_species_cutoff <- function(blast_res, dataset_select) {
+  blast_res %>%
+    filter(comp_type == "intra") %>%
+    filter(dataset == dataset_select) %>%
+    summarize(
+      mean_pident = mean(pident),
+      min_pident = min(pident),
+      .groups = "drop") %>%
+    mutate(dataset = dataset_select) %>%
+    pivot_longer(
+      names_to = "cutoff_type",
+      -dataset
+    )
+}
+
+test_blast <- function(
+  blast_res, dataset_select, cutoff_table, cutoff_type_select = "mean") {
+  intra_cutoff <-
+    cutoff_table %>%
+    filter(str_detect(dataset, "no_hybrids")) %>%
+    filter(str_detect(dataset, dataset_select)) %>%
+    filter(str_detect(cutoff_type, cutoff_type_select)) %>%
+    pull(value)
+
+  blast_res %>%
+    filter(dataset == dataset_select) %>%
+    mutate(
+      above_cutoff = pident > intra_cutoff,
+      match_other = q_species != s_species,
+      fail = above_cutoff & match_other) %>%
+    group_by(q_species) %>%
+    summarize(fail = any(fail), .groups = "drop") %>%
+    mutate(dataset = dataset_select)
+}
+
+summarize_blast_fail_rate <- function(blast_test_res_raw) {
+  blast_test_res_raw %>%
+    group_by(dataset) %>%
+    count(fail) %>%
+    ungroup()
 }
--- _targets.R
+++ _targets.R
@@ -1,35 +1,51 @@
 source("R/packages.R")
 source("R/functions.R")
 
-
 tar_plan(
-  # Load sequences ---
+  # Load data ---
+  # - rbcL sequences
   tar_file_read(
     rbcl_seqs_all,
-    "data_raw/BIFA_rbcL_analyses_240328.fasta",
+    "data_raw/BIFA_rbcL_analyses_240404.fasta",
     ape::read.FASTA(!!.x),
   ),
+  # - trnLF sequences
   tar_file_read(
     trnlf_seqs_all,
-    "data_raw/BIFA_trnLF_analyses_240328.fasta",
+    "data_raw/BIFA_trnLF_analyses_240404.fasta",
     ape::read.FASTA(!!.x),
+  ),
+  # - hybrid taxa
+  tar_file_read(
+    hybrid_taxa,
+    "data_raw/hybrid_taxa.txt",
+    read_lines(!!.x)
+  ),
+  # - species complexes
+  tar_file_read(
+    sp_complex,
+    "data_raw/species_complex.txt",
+    read_lines(!!.x)
   ),
   # - Drop specimens not identified to species
   rbcl_seqs = drop_indets(rbcl_seqs_all),
   trnlf_seqs = drop_indets(trnlf_seqs_all),
+  # - Also drop species complexes and hybrids
+  rbcl_seqs_no_hybrids = drop_complex_hybrids(
+    rbcl_seqs, sp_complex, hybrid_taxa),
+  trnlf_seqs_no_hybrids = drop_complex_hybrids(
+    trnlf_seqs, sp_complex, hybrid_taxa),
 
   # Align sequences ----
+  # Only do rbcL, since trnLF sequences are too diverged to align
   # - Align ingroup only
   rbcl_fern_align = align_seqs(rbcl_seqs),
-  trnlf_fern_align = align_seqs(trnlf_seqs),
   # - Align with outgroup
-  #   Only do rbcL, since trnLF sequences are too diverged to align
   rbcl_with_og = align_with_og(rbcl_seqs, marker = "rbcL"),
 
   # Gap test ----
   # Analyze genetic distances
   rbcl_dist = analyze_dist(rbcl_fern_align),
-  trnlf_dist = analyze_dist(trnlf_fern_align),
 
   # Monophyly test ----
   # - Build tree
@@ -50,6 +66,70 @@ tar_plan(
   # - Analyze monophyly
   rbcl_monophyly = check_monophy(rbcl_tree),
 
+  # BLAST test ----
+  # - create list for looping
+  seqs_list = list(
+    rbcl_seqs, trnlf_seqs, rbcl_seqs_no_hybrids, trnlf_seqs_no_hybrids),
+  seqs_names = c(
+    "rbcl", "trnlf", "rbcl_no_hybrids", "trnlf_no_hybrids"
+  ),
+  # - Write out sequences for BLAST db
+  tar_target(
+    seqs_for_blast_db,
+    write_fasta(
+      ape::del.gaps(seqs_list[[1]]),
+      paste0("_targets/user/blast/", seqs_names, ".fasta")
+    ),
+    format = "file",
+    pattern = map(seqs_list, seqs_names)
+  ),
+  # - Create BLAST databases
+  tar_target(
+    blast_db,
+    make_blast_db(
+      seqs_path = seqs_for_blast_db,
+      blast_db_dir = "_targets/user/blast/",
+      out_name = seqs_names),
+    format = "file",
+    pattern = map(seqs_for_blast_db, seqs_names)
+  ),
+  # - Run BLAST query
+  tar_target(
+    blast_res_tsv,
+    blast_n(
+      query = seqs_for_blast_db,
+      database = seqs_names,
+      out_file = paste0(seqs_names, "_blastn_results.tsv"),
+      wd = "_targets/user/blast/",
+      depends = blast_db
+    ),
+    format = "file",
+    pattern = map(seqs_for_blast_db, seqs_names, blast_db)
+  ),
+  tar_target(
+    blast_res_raw,
+    load_blast_tsv(blast_res_tsv, seqs_names),
+    pattern = map(blast_res_tsv, seqs_names)
+  ),
+  tar_target(
+    blast_res,
+    prep_blast_res(blast_res_raw),
+    pattern = map(blast_res_raw)
+  ),
+  tar_target(
+    cutoff_table,
+    get_species_cutoff(blast_res, seqs_names),
+    pattern = map(blast_res, seqs_names)
+  ),
+  tar_target(
+    blast_test_res_raw,
+    test_blast(
+      blast_res, seqs_names, cutoff_table,
+    ),
+    pattern = map(seqs_names)
+  ),
+  blast_fail_rate_summary = summarize_blast_fail_rate(blast_test_res_raw),
+  # - Load BLAST query results
   # Write report ----
   tarchetypes::tar_quarto(
     report
